4f8160ce545968c5d936afbd2f07d2c442d02f0b
morris <mnjoroge25439@gmail.com>
2023-11-19 02:44:07 -0800

    sotrd

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,74 +1,68 @@
 #include "sort.h"
-#include <stdlib.h>
 
 /**
-* counting_sort - Sorts an array of integers in ascending order
-*                 using the Counting sort algorithm
-*
-* @array: The array to be sorted
-* @size: Number of elements in the array
-*/
+ * get_max - Get the maximum value in an array of integers.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ *
+ * Return: The maximum integer in the array.
+ */
+int get_max(int *array, int size)
+{
+	int max, i;
+
+	for (max = array[0], i = 1; i < size; i++)
+	{
+		if (array[i] > max)
+			max = array[i];
+	}
+
+	return (max);
+}
+
+/**
+ * counting_sort - Sort an array of integers in ascending order
+ *                 using the counting sort algorithm.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ *
+ * Description: Prints the counting array after setting it up.
+ */
 void counting_sort(int *array, size_t size)
 {
-int *count_array, *output_array;
-int max, i;
+	int *count, *sorted, max, i;
 
-if (array == NULL || size < 2)
-return;
+	if (array == NULL || size < 2)
+		return;
 
-max = array[0];
+	sorted = malloc(sizeof(int) * size);
+	if (sorted == NULL)
+		return;
+	max = get_max(array, size);
+	count = malloc(sizeof(int) * (max + 1));
+	if (count == NULL)
+	{
+		free(sorted);
+		return;
+	}
 
-/* Find the maximum value in the array */
-for (i = 1; i < (int)size; ++i)
-{
-if (array[i] > max)
-max = array[i];
+	for (i = 0; i < (max + 1); i++)
+		count[i] = 0;
+	for (i = 0; i < (int)size; i++)
+		count[array[i]] += 1;
+	for (i = 0; i < (max + 1); i++)
+		count[i] += count[i - 1];
+	print_array(count, max + 1);
+
+	for (i = 0; i < (int)size; i++)
+	{
+		sorted[count[array[i]] - 1] = array[i];
+		count[array[i]] -= 1;
+	}
+
+	for (i = 0; i < (int)size; i++)
+		array[i] = sorted[i];
+
+	free(sorted);
+	free(count);
 }
-
-/* Allocate memory for the counting array */
-count_array = malloc((max + 1) * sizeof(int));
-if (count_array == NULL)
-return;
-
-/* Initialize the counting array to 0 */
-for (i = 0; i <= max; ++i)
-count_array[i] = 0;
-
-/* Count occurrences of each element in the array */
-for (i = 0; i < (int)size; ++i)
-count_array[array[i]]++;
-
-/* Print the counting array */
-print_array(count_array, max + 1);
-
-/* Update the counting array to store the cumulative count */
-for (i = 1; i <= max; ++i)
-count_array[i] += count_array[i - 1];
-
-/* Allocate memory for the output array */
-output_array = malloc(size * sizeof(int));
-if (output_array == NULL)
-{
-free(count_array);
-return;
-}
-
-/* Build the output array using the counting array */
-for (i = (int)size - 1; i >= 0; --i)
-{
-output_array[count_array[array[i]] - 1] = array[i];
-count_array[array[i]]--;
-}
-
-/* Copy the sorted elements back to the original array */
-for (i = 0; i < (int)size; ++i)
-array[i] = output_array[i];
-
-/* Print the sorted array */
-print_array(array, size);
-
-/* Free allocated memory */
-free(count_array);
-free(output_array);
-}
-
